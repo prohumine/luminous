009ee057914990248e47e4a8eacf984c240393bf
Marvin Santizo <santizomarvin88@gmail.com>
2015-05-14 15:44:37 -0600

    added flash notice for successful sign ups

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,6 +12,7 @@ class UsersController < ApplicationController
   	@user = User.new(user_params)
   	if @user.save
   		redirect_to @user
+      flash[:succes] = "Successfully signed up welcome #{@user.name}"
   	else
   		render 'new'
   	end
