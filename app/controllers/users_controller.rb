73f3815162d9d3630ec2a4709c82bbac9d0a1c10
Marvin Santizo <santizomarvin88@gmail.com>
2015-05-21 15:36:29 -0600

    added new file to integration test to test users edit and update; added before filters to make sure correct user is logged in to make changes to edit page and make sure correct user is making the changes. Added second user to fixtures to test, also added allow blank to password validation just for edit page.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
   include SessionsHelper
+  before_action :logged_in_user, only: [:edit, :update]
+  before_action :correct_user,   only: [:edit, :update]
 
   def index
   	@users = User.all
@@ -13,15 +15,25 @@ class UsersController < ApplicationController
   	@user = User.new(user_params)
   	if @user.save
       log_in @user
-      
-  		redirect_to @user
       flash[:success] = "Successfully signed up welcome #{@user.name}"
+  		redirect_to @user
   	else
   		render 'new'
   	end
   end
 
   def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      flash[:success] = "User updated"
+      redirect_to @user
+    else
+      render 'edit'
+    end
   end
 
   def show
@@ -32,5 +44,17 @@ class UsersController < ApplicationController
 
   def user_params
   	params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+
+  def logged_in_user
+    unless logged_in?
+      flash[:danger] = "Oh snap! You must be logged in to perform aciton"
+      redirect_to login_url
+    end
+  end
+
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(root_url) unless current_user?(@user)
   end
 end
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -2,6 +2,11 @@ require 'test_helper'
 
 class UsersControllerTest < ActionController::TestCase
   
+  def setup
+    @user = users(:michael)
+    @other_user = users(:archer)
+  end
+
   test "should get index" do
     get :index
     assert_response :success
@@ -12,13 +17,34 @@ class UsersControllerTest < ActionController::TestCase
     assert_response :success
   end
 
-  test "should get edit" do
-    get :edit, id: 1
-    assert_response :success
+  test "should redirect edit when not logged in" do
+    get :edit, id: @user
+    assert_not flash.empty?
+    assert_redirected_to login_url
   end
 
-  test "should get show" do
-    get :show, id: 1
-    assert_response :success
+  test "should redirect update when not logged in" do
+    patch :update, id: @user, user: { name: @user.name, email: @user.email }
+    assert_not flash.empty?
+    assert_redirected_to login_url
   end
+
+  test "should redirect edit when logged in as wrong user" do
+    log_in_as(@other_user)
+    get :edit, id: @user
+    assert flash.empty?
+    assert_redirected_to root_url
+  end
+
+  test "should redirect update when logged in as wrong user" do
+    log_in_as(@other_user)
+    patch :update, id: @user, user: { name: @user.name, email: @user.email }
+    assert flash.empty?
+    assert_redirected_to root_url
+  end
+
+  # test "should get show" do
+  #   get :show, id: @user
+  #   assert_response :success
+  # end
 end
