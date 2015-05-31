ceb7c3e8b70918ead2d6e48d735063e7488021f8
Marvin Santizo <santizomarvin88@gmail.com>
2015-05-31 17:36:51 -0600

    Added warning alert to stylesheets; added more logic to account_activations controller to check find user by email in params, check to see if user is authenticated and to check if user has already been activated, if it's all true it will log user in and flash success, else redirect to root. similar in sessions controller, checks to see if user is activated, if not it will flash warning check email for activation link and redirect to root. changed Aunthenticated? method to accept 2 arguments attribute and token changed current_user method and model test to reflect change. Changed config route to localhost instead of luminous.com.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,9 +7,15 @@ class SessionsController < ApplicationController
   def create
   	user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
-      log_in user
-      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
-      redirect_back_or user
+      if user.activated?
+        log_in user
+        params[:session][:remember_me] == '1' ? remember(user) : forget(user)
+        redirect_back_or user
+      else
+        message  = 'Account not activated'
+        message += 'Check your email for the activation link'
+        flash[:warning] = message
+        redirect_to root_url
     else
       flash.now[:danger] = "Oh snap! Better check yourself, you're not looking to good."
       render 'new'
